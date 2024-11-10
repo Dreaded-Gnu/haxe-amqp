cc4cffccc56c722cc3cf52b53e390144e60f265f
Christian Freitag <cfreitag.cf@googlemail.com>
2024-11-10 19:47:11 +0100

    - Readded promises as submodule - Added macro to manipulate the class path and adds src and promises src

---
+++ macro-src/AmqpSrc.hx
@@ -0,0 +1,22 @@
+#if macro
+
+import haxe.macro.Compiler;
+import haxe.macro.Context;
+import sys.FileSystem;
+
+class AmqpSrc {
+  public static function apply() {
+    for (cp in Context.getClassPath()) {
+      var path:String = FileSystem.absolutePath(cp);
+      var index:Int = path.indexOf('haxe-amqp/macro-src');
+      var index2:Int = path.indexOf('macro-src');
+      if (index != -1 && index2 != -1) {
+        Compiler.addClassPath(path.substr(0, index2) + 'src');
+        Compiler.addClassPath(path.substr(0, index2) + 'src/promises/src');
+        break;
+      }
+    }
+  }
+}
+
+#end
