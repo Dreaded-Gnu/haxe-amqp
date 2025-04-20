420195989ba1ee44d095f87d51026dacbd30db33
Christian Freitag <cfreitag.cf@googlemail.com>
2025-04-20 17:59:43 +0200

    Updated publish script and haxelib.json
    - Added .zip to gitignore list
    - Renamed haxe-amqp to amqp in haxelib.json
    - Revised publish script to build license on demand out of license and promises submodule license

--- publish.sh
+++ publish.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
-rm -f haxe-amqp.zip
-zip -r haxe-amqp.zip src macro-src README.md CHANGELOG.md LICENSE.md haxelib.json extraParams.hxml
-haxelib submit haxe-amqp.zip
+rm -f amqp.zip
+mv LICENSE.md LICENSE.md.bak
+cat LICENSE.md.bak src/promises/LICENSE > LICENSE.md
+zip -r amqp.zip src macro-src README.md CHANGELOG.md LICENSE.md haxelib.json extraParams.hxml
+rm LICENSE.md
+mv LICENSE.md.bak LICENSE.md
+haxelib submit amqp.zip
