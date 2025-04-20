fed295eeb821f3a60280b8b5c9fdca1679f95582
Christian Freitag <cfreitag.cf@googlemail.com>
2025-04-20 15:04:39 +0200

    Updated publish script
    - Updated publish shell script by removing username from haxelib submit command

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 rm -f haxe-amqp.zip
 zip -r haxe-amqp.zip src macro-src README.md CHANGELOG.md LICENSE.md haxelib.json extraParams.hxml
-haxelib submit haxe-amqp.zip DreadedGnu
+haxelib submit haxe-amqp.zip
